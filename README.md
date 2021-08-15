f3b45c6e8bfeb77145c0a08a933c42deff988870
jayskhatri <jaykhatri.jsk@gmail.com>
2021-08-15 13:55:21 +0530

    slight changes

--- README.md
+++ README.md
@@ -4,18 +4,24 @@
 ## AIM: 
 We are given i/p of audio file which contains yes and no as spoken word. We have to write the code which identify the words yes and no.
 
-## Process:
-1. I have recorded the audio for finding the dc shift occuring in my laptop after decreasing the microphone sensitivity, which is in [dc_shift.txt](https://github.com/jayskhatri/yes-no-classifier-using-audio-freq/blob/main/assn1/dc_shift.txt) file.
-2. Also recorded the ambience file to find out average noise energy and average zcr value which is [n_ambience.txt](https://github.com/jayskhatri/yes-no-classifier-using-audio-freq/blob/main/assn1/n_ambience.txt). Recorded YES/NO file in the same environment which is [yes_no.txt]().
-3. Now, first we find out the max value occuring in out yes_no.txt audio. that max value we will use for the normalization our data into +/- 5000.
-4. setupGlobal() function is called to setting up the values of the dcShift, max (max value from the raw audio file), normalization factor.
-5. Now we normalize our ambience data file as well as yes_no recorded data file to +/- 5000 using the global values we have set up.
-6. Now, using the normalized ambience data file, I have find the average values of the noise energy and zcr.
-7. using that average value, I have taken the multiplier value as 5 (found from tuning).
-8. lastly we call the word_seggregation() function to mark the start and end of the spoken word from the file, which subsequently call the processWord() function which analyse the spoken word from start marker to end marker and echo the decision.   
-
 # Steps to run the project
 1. Download zip and extract it
 2. Open Microsoft visual studio and press ```Ctrl + Shift + O``` and browse to the folder where you extracted the zip.
 3. select the **.sln** file, it will launch the project.
 4. Go to Source files and select assn1.cpp file and press F5. 
+
+## Process:
+1. I have recorded the audio for finding the dc shift occuring in my laptop after decreasing the microphone sensitivity, which is in [dc_shift.txt](https://github.com/jayskhatri/yes-no-classifier-using-audio-freq/blob/main/assn1/dc_shift.txt) file.
+2. Also recorded the ambience file to find out average noise energy and average zcr value which is [ambience.txt](https://github.com/jayskhatri/yes-no-classifier-using-audio-freq/blob/main/assn1/n_ambience.txt). Recorded YES/NO file in the same environment which is [yes_no.txt](https://github.com/jayskhatri/yes-no-classifier-using-audio-freq/blob/main/assn1/yes_no.txt). The file which was provided to us in facebook is [provided_yes_no.txt](https://github.com/jayskhatri/yes-no-classifier-using-audio-freq/blob/main/assn1/provided_yes_no.txt).
+3. Now, first we find out the max value occuring in out yes_no.txt audio. that max value we will use for the normalization our data into +/- 5000.
+4. ```setupGlobal()``` function is called to setting up the values of the dcShift, max (max value from the raw audio file), normalization factor.
+5. Now we normalize our ambience data file as well as yes_no recorded data file to +/- 5000 using the global values we have set up. Function to normalize the data is ```normalize_data()```
+6. Now, using the normalized ambience data file, I have find the average values of the noise energy and zcr.
+7. using that average value, I have taken the multiplier value as 5 (found from tuning).
+8. lastly we call the ```word_seggregation()``` function to mark the start and end of the spoken word from the file, which subsequently call the ```processWord()``` function which analyse the spoken word from start marker to end marker and echo the decision.   
+
+# Logic
+- To identify the word start we check for the continuous 4 frames energy, if it is high then some threshold energy then we consider it as word start
+- To identify the word end we check for the continuous 4 frames enrgy, it it is low then some threshold energy then we consider it as word end
+- Threshold energy is 5*noise energy
+- Then we count the number of fricatives in that word, if it is more then the 25% of the word size, then it is considered as YES, else NO
--- assn1/assn1.cpp
+++ assn1/assn1.cpp
@@ -11,7 +11,7 @@
 
 //constants
 //threshold multiplier value is taken after tuning
-const int noFrames = 150, thrsld_multiplier = 5;
+const int noSamplesPerFrame = 150, thrsldMultiplier = 5;
 const double limit = 5000.0;
 
 //global variables
@@ -19,7 +19,7 @@ long int max;
 double dcShift, nFactor;
 
 //function to get dcshift value and set in global variable
-double getDCShift(){
+double get_dc_shift(){
 
     long int sample_count = 0;
     FILE *fp;
@@ -45,7 +45,7 @@ double getDCShift(){
 
 //function to setup the global variable like, max and nFactor
 //max and nFactor depends on the main yes/no word file and are used to do the normalization
-void setupGlobal(char* filename){
+void setup_global(char* filename){
     FILE *fp;
     long int totalSample = 0;
     char line[80];
@@ -70,7 +70,7 @@ void setupGlobal(char* filename){
     nFactor = (double)limit/max;
     
     //setup dcShift
-    getDCShift();
+    get_dc_shift();
     fclose(fp);
 }
 
@@ -122,7 +122,7 @@ void normalize_data(char* inputFileName, char* outputFileName){
 
 //this function finds out the average energy and zcr value of ambience
 // we record ambience sound beforehand in the same environment, so we can use that for identifying whether word spoken or not
-double* countAvgAmbience(char* inputFileName){
+double* count_avg_ambience(char* inputFileName){
     FILE *fp, *op;
     long int total_frame = 0;
     int n = 0;
@@ -135,7 +135,7 @@ double* countAvgAmbience(char* inputFileName){
         exit(1);
     }
 
-    double *avg = (double*)malloc(sizeof(double)*2);
+    double *avg = (double*) malloc(sizeof(double)*2);
     avg[0] = 0.0;
     avg[1] = 0.0;
     
@@ -144,9 +144,9 @@ double* countAvgAmbience(char* inputFileName){
         fgets(line, 80, fp);
         if(!isalpha(line[0])){
             x = atof(line);
-            if(n == noFrames){
+            if(n == noSamplesPerFrame){
                 total_frame++;
-                avg[0] += energy/noFrames;
+                avg[0] += energy/noSamplesPerFrame;
                 avg[1] += zcr;
 
                 energy = 0.0;
@@ -167,7 +167,7 @@ double* countAvgAmbience(char* inputFileName){
 }
 
 //this function process the word using energy and zcr and echo the decision, whether the word is YES or NO
-void processWord(long int word_s, long int word_e, double energy[], double zcr[]){
+void process_word(long int word_s, long int word_e, double energy[], double zcr[]){
     int size = word_e - word_s + 1;
 
     //counting fricatives
@@ -215,9 +215,9 @@ void word_seggregation(double* ambAvg, char* inputFileName){
             x = atof(line);
             
             if(abs(x) > 1){
-                if(n == noFrames){
+                if(n == noSamplesPerFrame){
                     //avg energy
-                    en /= noFrames;
+                    en /= noSamplesPerFrame;
                     total_frame++;
                     energy[lastIndex] = en;
                     zcr[lastIndex++] = z;
@@ -238,18 +238,18 @@ void word_seggregation(double* ambAvg, char* inputFileName){
     printf("total frames: %ld\n\n", lastIndex);
 	printf("Recognized words\n");
 	
-    for( ; i<lastIndex-3; ++i){
-		if(word_start == -1 && word_end == -1 && energy[i+1] > ambAvg[0] * thrsld_multiplier && energy[i+2] > ambAvg[0] * thrsld_multiplier && energy[i+3] > ambAvg[0] * thrsld_multiplier){
+    for( ; i<lastIndex-4; ++i){
+		if(word_start == -1 && word_end == -1 && energy[i+1] > ambAvg[0] * thrsldMultiplier && energy[i+2] > ambAvg[0] * thrsldMultiplier && energy[i+3] > ambAvg[0] * thrsldMultiplier && energy[i+4] > ambAvg[0] * thrsldMultiplier){//
 			//marking word start
             word_start = i;
 		}
-		else if(word_start != -1 && word_end == -1 && energy[i+1] <= ambAvg[0] * thrsld_multiplier && energy[i+2] <= ambAvg[0] * thrsld_multiplier && energy[i+3] <= ambAvg[0] * thrsld_multiplier){
+		else if(word_start != -1 && word_end == -1 && energy[i+1] <= ambAvg[0] * thrsldMultiplier && energy[i+2] <= ambAvg[0] * thrsldMultiplier && energy[i+3] <= ambAvg[0] * thrsldMultiplier && energy[i+4] <= ambAvg[0] * thrsldMultiplier){
             //marking word end
             word_end = i;
-			//printf("start : %ld, end: %ld, energy at start: %lf & end: %lf\nword_start: %ld, word_end: %ld", word_start*noFrames, word_end*noFrames, energy[word_start], energy[word_end], word_start, word_end);
+			//printf("start : %ld, end: %ld, energy at start: %lf & end: %lf\nword_start: %ld, word_end: %ld", word_start*noSamplesPerFrame, word_end*noSamplesPerFrame, energy[word_start], energy[word_end], word_start, word_end);
 			printf("Word %d: ", ++totalWord);
             //processing the word
-            processWord(word_start, word_end, energy, zcr);
+            process_word(word_start, word_end, energy, zcr);
 			
             //processing of word is completed, so we mark the markers to -1, to reuse it
             word_start = -1;
@@ -261,16 +261,16 @@ void word_seggregation(double* ambAvg, char* inputFileName){
 int _tmain(int argc, _TCHAR* argv[])
 {
     //setup global values according to input
-	setupGlobal("yes_no.txt");
+	setup_global("provided_yes_no.txt");
 
     //normalizing the input word data
-    normalize_data("yes_no.txt", "normalized_yes_no.txt");
+    normalize_data("provided_yes_no.txt", "normalized_yes_no.txt");
 
     //normalizing the ambience data
     normalize_data("ambience.txt", "normalized_ambience.txt");
 
     //counting avg energy and zcr of the normalized ambience data
-    double* ambience_avg = countAvgAmbience("normalized_ambience.txt");
+    double* ambience_avg = count_avg_ambience("normalized_ambience.txt");
     printf("ambience avg energy: %lf, avg zcr: %lf\n\n", ambience_avg[0], ambience_avg[1]);
     
     //calling function to seggregate the words and echo decision
